1edb458d279d1bc0a29ca613869cfbda7891d909
Thabang Gideon Magaola <gideon877@live.com>
2025-01-16 18:23:27 +0200

    more taxi route api

--- src/api/RouteApi.ts
+++ src/api/RouteApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { TaxiRouteParams } from "../utils/interfaces";
+import { RouteDetails, TaxiRouteParams, UpdateTaxiRouteParams } from "../utils/interfaces";
 
 const BASE_URL = `${import.meta.env.VITE_REACT_APP_API_URL}/taxi-routes`;
 
@@ -23,4 +23,35 @@ export const createTaxiRoute = async (params: TaxiRouteParams) => {
         console.error(`Error creating route for rank id: ${params.fromRankId}`, error);
         throw error;
     }
+}
+
+export const updateTaxiRoute = async (params: UpdateTaxiRouteParams) => {
+    try {
+        const response = await axios.patch(`${BASE_URL}/${params.routeId}`, params);
+        return response.data;
+    } catch (error) {
+        console.error(`Error updating route with id: ${params.routeId}`, error);
+        throw error;
+    }
+}
+
+export const deleteTaxiRoute = async (id: number) => {
+    try {
+        const response = await axios.delete(`${BASE_URL}/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error deleting route with id: ${id}`, error)
+        throw error;
+    }
+}
+
+export const getRouteDetailsByQueueId = async (queueId: number): Promise<RouteDetails> => {
+    try {
+        const response = await axios.get(`${BASE_URL}/route-details/${queueId}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching route details for queue with id: ${queueId}`, error);
+        throw error;
+    }
+
 }
